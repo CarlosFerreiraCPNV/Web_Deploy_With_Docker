8c1b7f4662ac4c4cdf1a7d7e408d621467c452d4
Carlos Ferreira <150017797+CarlosFerreiraCPNV@users.noreply.github.com>
2024-11-01 08:27:50 +0100

    Delete project-root directory

--- project-root/scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Initialisation des conteneurs..."
-docker-compose down
-docker-compose up -d
-
-echo "Initialisation terminée. Accédez à l'application sur http://localhost:8080"
